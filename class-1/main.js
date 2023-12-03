c0b54ebb5cc47275883f696a85dc507e41067708
aditya <adityadewhy@gmail.com>
2023-12-03 19:54:08 +0530

    initial setup

---
+++ class-1/main.js
@@ -0,0 +1,19 @@
+console.log("hello world")
+
+// cluster module to run programs parallely
+// google above line and subroutines
+
+let fname ="aditya"
+let lname = "sharma"
+console.log(fname+lname);
+
+let gender = "m"
+if(gender == "m"){
+    console.log("hello male");
+} else{
+    console.log("hello girl");
+}
+
+for(let i = 0 ; i<= 1000; i++){
+    console.log(i);
+}
