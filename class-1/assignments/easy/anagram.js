f60a965e02b428e1bc07b0d418341716ead7a5f4
aditya <adityadewhy@gmail.com>
2023-12-05 00:07:04 +0530

    cant do here, rest in assignments repo

--- class-1/assignments/easy/anagram.js
+++ class-1/assignments/easy/anagram.js
@@ -5,7 +5,28 @@
 */
 
 function isAnagram(str1, str2) {
+    let l1 =[];
+    let l2 = [];
+    for(let i = 0; i < str1.length ; i++){
+        l1.push(str1.charAt(i));
+    }
+    for(let i = 0; i < str2.length ; i++){
+        l2.push(str2.charAt(i));
+    }
+    l1.sort();
+    l2.sort();
 
+    let sortedStr1 = l1.join('');
+    let sortedStr2 = l2.join('');
+
+    if(sortedStr1 == sortedStr2){
+        return true;
+    }else{
+        return false;
+    }
 }
-
+    
+  
+isAnagram("openpi", "aiopen"); // Output: true
+  
 module.exports = isAnagram;
