85a868ab4928280b8dca32cbe7d503a051414005
aditya <adityadewhy@gmail.com>
2024-04-02 13:07:15 +0530

    types added

--- w5/new-year-app/backend/index.js
+++ w5/new-year-app/backend/index.js
@@ -2,8 +2,35 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const { createTodo } = require("./types");
+
+
+app.use(express.json());
+
+
 app.get('/',(req,res)=>{
     res.send('hello ji');
+})
+
+app.post("/todo",function(req,res){
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "fix the error first",
+        })
+        return;
+    }
+})
+
+app.get("/todos",function(req,res){
+    const updatePayload = req.body;
+    const parse
+
+})
+
+app.post("/done",function(req,res){
+    
 })
 
 app.listen(port,()=>{
---
+++ w5/new-year-app/backend/types.js
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+const createTodo = z.object ({
+    title : z.string(),
+    desc : z.string(),
+})
+
+const updateTodo = z.object({
+    id: z.string(),
+})
+
+module.exports = {
+    createTodo: createTodo,
+    updateTodo: updateTodo,
+}
