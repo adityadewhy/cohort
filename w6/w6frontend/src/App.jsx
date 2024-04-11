a508fb8ba1608f85d892b1cd98f724081d261673
aditya <adityadewhy@gmail.com>
2024-04-12 02:22:09 +0530

    learned children prop

--- w6/w6frontend/src/App.jsx
+++ w6/w6frontend/src/App.jsx
@@ -1,50 +1,19 @@
 import React, {useState} from "react";
 
+let counter = 4;
+
 function App() {
-  const [items, setItem] = useState([{
-    id: 1,
-    title : 'add first ',
-    desc: ' add frist ka desc',
-  }, {
-    id: 2,
-    title: 'second todo ',
-    desc: 'second todo ka desc',
-  }, {
-    id: 3,
-    title: "third todo",
-    desc: "third todo ka desc",
-  }])
-
-
-  function handleClick(){
-    setItem([...items, {
-      id: 4,
-      title : Math.random(),
-      desc: Math.random(),
-    }])
-  }
-
-	return (
-		<div>
-      <button onClick={handleClick}> add new todo </button>
-			{items.map( item => <Todo title= {item.title} desc= {item.desc} />)}
-		</div>
-	);
+	return <div>
+    <CardWrapper>
+      this is the child here
+    </CardWrapper>
+  </div>;
 }
 
-function Todo(props){
+function CardWrapper({children}){
   return(
-    <div>
-
-      
-      <h1>
-        {props.title}
-      </h1> <br/>
-
-      <h3>
-        {props.desc}
-      </h3> <br/>
-
+    <div style={{border:"2px solid red"}}>
+      {children}
     </div>
   )
 }
