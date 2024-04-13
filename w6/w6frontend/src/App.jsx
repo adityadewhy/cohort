c62c9c2241e8408fd15023d962764f26d6e6b4f5
aditya <adityadewhy@gmail.com>
2024-04-14 04:12:39 +0530

    useEffect is working fine now

--- w6/w6frontend/src/App.jsx
+++ w6/w6frontend/src/App.jsx
@@ -1,21 +1,31 @@
-import React, {useState} from "react";
-
-let counter = 4;
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
-	return <div>
-    <CardWrapper>
-      this is the child here
-    </CardWrapper>
-  </div>;
+	return (
+		<div>
+			<Todo id={3} />
+		</div>
+	);
 }
 
-function CardWrapper({children}){
-  return(
-    <div style={{border:"2px solid red"}}>
-      {children}
-    </div>
-  )
+function Todo(props) {
+	const [todos, setTodos] = useState({});
+
+	useEffect(() => {
+		axios
+			.get("https://sum-server.100xdevs.com/todo?id=" + props.id)
+			.then(function (res) {
+				setTodos(res.data.todo);
+			});
+	},[]);
+
+	return (
+		<div>
+			<h1>{todos.title}</h1>
+			<h5>{todos.description}</h5>
+		</div>
+	);
 }
 
 export default App;
